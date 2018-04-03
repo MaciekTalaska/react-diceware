3d83174be48cfb324f8a14632e3316dac7d046e8
MaciekTalaska <maciek.talaska@gmail.com>
2018-04-03 20:35:50 +0200

    use prettier on .js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 import './style.css';
 
 // list of words taken from: https://www.eff.org/files/2016/09/08/eff_short_wordlist_2_0.txt
@@ -15,46 +15,45 @@ class Dice {
     let numbers = new Uint32Array(dices);
     window.crypto.getRandomValues(numbers);
 
-		let array = Array.from(numbers);
-		return Dice.dicesToKey(array);
+    let array = Array.from(numbers);
+    return Dice.dicesToKey(array);
   }
 
   static truncate(n) {
     if (n > 6) {
-      return (n % 6 + 1);
+      return n % 6 + 1;
     } else {
       return n;
     }
   }
 
-	static dicesToKey(numbers) {
-		let array = numbers.map((n) => Dice.truncate(n));
-		if (array.length > diceCount) {
-			array.length = 4; 
-		}
-		let key = array.join('');
-		return key;
-	}
+  static dicesToKey(numbers) {
+    let array = numbers.map(n => Dice.truncate(n));
+    if (array.length > diceCount) {
+      array.length = 4;
+    }
+    let key = array.join('');
+    return key;
+  }
 }
 
 class WordsRepository {
-  
   static loadWordsList() {
-		return new Promise( (resolve, reject) => {
-			fetch(dicewareListUrl)
-			.then((res) => res.text())
-			.then((data) => {
-				let list = Array.from(data.split(/\n/));
-				let map = new Map();
+    return new Promise((resolve, reject) => {
+      fetch(dicewareListUrl)
+        .then(res => res.text())
+        .then(data => {
+          let list = Array.from(data.split(/\n/));
+          let map = new Map();
 
-				list.forEach((line) => {
-					let [k,v] = line.split(/\t/);
-					map.set(k,v);
-				});
-				resolve(map);	
-			});
-		});
-	}
+          list.forEach(line => {
+            let [k, v] = line.split(/\t/);
+            map.set(k, v);
+          });
+          resolve(map);
+        });
+    });
+  }
 }
 
 class DiceButton extends Component {
@@ -68,24 +67,19 @@ class DiceButton extends Component {
   }
 
   render() {
-    return (
-      <button onClick={this.forceRefresh}>Generate!</button>
-    );
+    return <button onClick={this.forceRefresh}>Generate!</button>;
   }
 }
 
 class DiceWord extends Component {
-  
   constructor(props) {
     super(props);
-		console.log('ths.props.word: ', this.props.word);
+    console.log('ths.props.word: ', this.props.word);
   }
 
   render() {
-		console.log('[DiceWord.render] this.props.word: ', this.props.word);
-    return (
-			(<h2>Current word is: {this.props.word}</h2>)
-    );
+    console.log('[DiceWord.render] this.props.word: ', this.props.word);
+    return <h2>Current word is: {this.props.word}</h2>;
   }
 }
 
@@ -94,31 +88,31 @@ class Diceware extends Component {
     super(props);
     this.state = {
       numbers: [],
-			list : null,
-			word: null
+      list: null,
+      word: null,
     };
     this.generateNewWord = this.generateNewWord.bind(this);
   }
-	
-	componentWillMount() {
-		WordsRepository.loadWordsList().then( (result) => {
-			this.setState({list: result});
-			this.generateNewWord();
-		});		
-	}
+
+  componentWillMount() {
+    WordsRepository.loadWordsList().then(result => {
+      this.setState({list: result});
+      this.generateNewWord();
+    });
+  }
 
   generateNewWord() {
-		let key = Dice.rollDices(diceCount);
-		this.setState({numbers: key});
-			let newWord = this.state.list.get(key);
-			this.setState({word: newWord });
+    let key = Dice.rollDices(diceCount);
+    this.setState({numbers: key});
+    let newWord = this.state.list.get(key);
+    this.setState({word: newWord});
   }
 
   render() {
     return (
       <div>
-				<DiceWord word={this.state.word}/>
-        <DiceButton onNewNumberRequest={this.generateNewWord}/>
+        <DiceWord word={this.state.word} />
+        <DiceButton onNewNumberRequest={this.generateNewWord} />
       </div>
     );
   }
@@ -126,14 +120,11 @@ class Diceware extends Component {
 
 class ApplicationName extends Component {
   render() {
-    return (
-      <h1>Diceware Password Generator</h1>
-    );
+    return <h1>Diceware Password Generator</h1>;
   }
 }
 
 class App extends Component {
-
   render() {
     return (
       <div>
