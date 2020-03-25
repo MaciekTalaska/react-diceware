6d39cdf03672062f3f4b5a3d4ee3fdd79f13d088
MaciekTalaska <maciek.talaska@gmail.com>
2020-03-25 12:05:38 +0100

    Updates

---
+++ precache-manifest.800257de841cabe64f89eb5109af0875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319e2bb8f75dd8cd4c10108070c1d091",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cca81e5eb49a9ca5bbd6",
+    "url": "./static/css/main.67f028f6.chunk.css"
+  },
+  {
+    "revision": "6e39d604cbcf44fcbbeb",
+    "url": "./static/js/2.95b848fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.95b848fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca81e5eb49a9ca5bbd6",
+    "url": "./static/js/main.0536dced.chunk.js"
+  },
+  {
+    "revision": "e1f27bf5a085067765f7",
+    "url": "./static/js/runtime-main.a11fbccd.js"
+  }
+]);
