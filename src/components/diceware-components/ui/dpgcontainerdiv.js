7eedb80bdd109d27c7d207dc2578a295fd29b20e
MaciekTalaska <maciek.talaska@gmail.com>
2020-04-19 20:54:11 +0200

    more cleanup

--- src/components/diceware-components/ui/dpgcontainerdiv.js
+++ src/components/diceware-components/ui/dpgcontainerdiv.js
@@ -4,17 +4,7 @@ import PropTypes from "prop-types";
 
 class DpgContainerDiv extends Component {
   render() {
-    return (
-      //  this literally means: override default style assigned to div
-      //  with the style being passed to it
-      <div className={s.container}>{this.props.children}</div>
-      // TODO: this should be better done, not all properties set via
-      // default style should be lost
-      // actually these two should be somehow merged
-      // <div className={this.props.className ? this.props.className : styles.div}>
-      //   {this.props.children}
-      // </div>
-    );
+    return <div className={s.container}>{this.props.children}</div>;
   }
 }
 
